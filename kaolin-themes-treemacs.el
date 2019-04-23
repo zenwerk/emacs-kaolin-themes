7bdc682315063ffc31c9f29be34b3acc00f790ea
zenwerk <zenwerk@localhost>
2019-04-24 03:11:40 +0900

    change treemacs icon

--- kaolin-themes-treemacs.el
+++ kaolin-themes-treemacs.el
@@ -62,14 +62,14 @@
           treemacs-icon-text treemacs-icon-fallback)
 
     (setq treemacs-icon-root-png
-          (format " %s " (all-the-icons-material "subject" :v-adjust -0.2 :height 1.4
+          (format " %s " (all-the-icons-octicon "repo" :v-adjust -0.2 :height 1.4
                                                  :face 'font-lock-variable-name-face))
 
           treemacs-icon-open-png
-          (format "%s " (all-the-icons-material "folder_open" :v-adjust -0.2 :height 1.15 'font-lock-doc-face))
+          (format "%s " (all-the-icons-material "folder_open" :v-adjust -0.2 :height 1 'font-lock-doc-face))
 
           treemacs-icon-closed-png
-          (format "%s " (all-the-icons-material "folder" :v-adjust -0.2 :height 1.15))
+          (format "%s " (all-the-icons-material "folder" :v-adjust -0.2 :height 1))
 
           treemacs-icon-tag-open-png
           (all-the-icons-faicon "chevron-down" :v-adjust 0.1)
@@ -87,45 +87,55 @@
           (format "%s " (all-the-icons-faicon "tag" :height 0.9 :face 'font-lock-type-face))
           )
 
-    ;; Icons for filetypes
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "csharp-line") "cs")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "css3") "css")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "git") "gitignore")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "html5") "html" "htm")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "java") "java")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "javascript-badge") "js")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "python") "py")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "rust") "rs")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "haskell") "hs")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "c") "c")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "cplusplus") "cpp")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "ruby-alt") "rb")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "scala") "scala")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "elixir") "ex" "exs")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "erlang") "erl" "hrl")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "clojure") "clj" "cljs")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "cabal") "cabal")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "clisp") "lisp")
+    ;; Icons for filetypes with Color
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".cs") "cs")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".css") "css")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".git") "gitignore")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".html") "html" "htm")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".java") "java")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".js") "js")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".ts") "ts")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".py") "py")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".rs") "rs")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".hs") "hs")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".c") "c")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".cpp") "cpp" "C" "cc")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".rb") "rb")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".scala") "scala")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".ex") "ex")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".exs") "exs")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".erl") "hrl")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".hrl") "hrl")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".clj") "clj")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".cljs") "cljs")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".el") "el" "elc")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".org") "org")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".jl") "jl")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".ml") "ml")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".mli") "mli")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".cl") "lisp")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".php") "php")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".md") "md" "markdown")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".sh") "sh")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".zsh") "zsh")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".jsx") "jsx")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file ".vue") "vue")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file "Makefile") "Makefile")
+    (treemacs-define-custom-icon (all-the-icons-icon-for-file "Dockerfile") "Dockerfile")
+    ;; Icons with no color
     (treemacs-define-custom-icon (all-the-icons-fileicon "go") "go")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "elisp") "el" "elc")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "julia") "jl")
+    (treemacs-define-custom-icon (all-the-icons-fileicon "cabal") "cabal")
     (treemacs-define-custom-icon (all-the-icons-fileicon "kotlin") "kt" "kts")
     (treemacs-define-custom-icon (all-the-icons-fileicon "hy") "hy")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "jsx2-alt") "jsx")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "ocaml") "ml" "mli")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "org") "org")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "php") "php")
-    (treemacs-define-custom-icon (all-the-icons-alltheicon "terminal") "sh" "zsh")
     (treemacs-define-custom-icon (all-the-icons-fileicon "typescript") "ts")
     (treemacs-define-custom-icon (all-the-icons-fileicon "nimrod") "nim" "nims")
     (treemacs-define-custom-icon (all-the-icons-fileicon "perl6") "pm6")
     (treemacs-define-custom-icon (all-the-icons-fileicon "tex") "tex")
     (treemacs-define-custom-icon (all-the-icons-fileicon "rst") "rst")
-    (treemacs-define-custom-icon (all-the-icons-fileicon "vue") "vue")
-    (treemacs-define-custom-icon (all-the-icons-octicon "markdown") "md" "markdown")
     (treemacs-define-custom-icon (all-the-icons-octicon "file-pdf") "pdf")
     (treemacs-define-custom-icon (all-the-icons-octicon "database") "sql")
     (treemacs-define-custom-icon (all-the-icons-material "style") "styles")
+
 
     (treemacs-define-custom-icon (all-the-icons-octicon "file-media")
                                  "jpg" "jpeg" "png" "gif" "ico" "tif" "tiff" "svg" "bmp"
@@ -142,8 +152,8 @@
                                  "tpl" "erb" "mustache" "twig" "ejs" "mk" "haml" "pug" "jade")
 
     (treemacs-define-custom-icon (all-the-icons-octicon "file-zip")
-                                 "zip" "xz" "tar" "7z" "rar"))
-  )
+                                 "zip" "xz" "tar" "7z" "rar")
+  ))
 
 
 (provide 'kaolin-themes-treemacs)
